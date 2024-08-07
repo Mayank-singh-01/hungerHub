f8f57ee9b58390f5dbf0236ec116f32faee3e147
Mayank Singh <mayank23378@gmail.com>
2024-08-07 17:04:27 +0530

    seatup file for frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
